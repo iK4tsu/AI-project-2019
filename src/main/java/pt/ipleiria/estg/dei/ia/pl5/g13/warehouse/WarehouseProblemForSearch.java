8d0c6ba81a5fec8c472f93ee75843f1516befe0a
João Lourenço <jlourenco5691@gmail.com>
2020-04-04 20:55:22 +0100

    warehouseProblemForSearch: fixed class properties and methods
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- src/main/java/pt/ipleiria/estg/dei/ia/pl5/g13/warehouse/WarehouseProblemForSearch.java
+++ src/main/java/pt/ipleiria/estg/dei/ia/pl5/g13/warehouse/WarehouseProblemForSearch.java
@@ -1,28 +1,45 @@
 package pt.ipleiria.estg.dei.ia.pl5.g13.warehouse;
 
+import pt.ipleiria.estg.dei.ia.pl5.g13.agentSearch.Action;
 import pt.ipleiria.estg.dei.ia.pl5.g13.agentSearch.Problem;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class WarehouseProblemForSearch<S extends WarehouseState> extends Problem<S> {
-
-    //TODO: this class might require the definition of additional methods and/or attributes
+    private LinkedList<Action> actions;
+    private final Cell goalPosition;
 
     public WarehouseProblemForSearch(S initialWarehouseState, Cell goalPosition) {
         super(initialWarehouseState);
-
-        //TODO:
-        throw new UnsupportedOperationException("Not implemented yet.");
+        actions = new LinkedList<>();
+        actions.add(new ActionDown());
+        actions.add(new ActionLeft());
+        actions.add(new ActionRight());
+        actions.add(new ActionUp());
+        this.goalPosition = goalPosition;
     }
 
     @Override
     public List<S> executeActions(S state) {
-        //TODO:
-        throw new UnsupportedOperationException("Not implemented yet.");
+        LinkedList<S> ret = new LinkedList<>();
+
+        for (Action action: actions)
+        {
+            if (action.isValid(state))
+            {
+                S successor = (S) state.clone();
+                action.execute(successor);
+                ret.add(successor);
+            }
+        }
+
+        return ret;
     }
 
+    // determine if all the requests are completed
+    @Override
     public boolean isGoal(S state) {
-        //TODO:
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return state.equalsAgent(goalPosition);
     }
 }
