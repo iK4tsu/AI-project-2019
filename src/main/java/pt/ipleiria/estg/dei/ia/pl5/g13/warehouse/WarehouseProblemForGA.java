2476e18afac52a5529a60cef5925f657582b1923
João Lourenço <jlourenco5691@gmail.com>
2020-04-04 23:36:13 +0100

    warehouseProblemForGA: fixed methods and ctor
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- src/main/java/pt/ipleiria/estg/dei/ia/pl5/g13/warehouse/WarehouseProblemForGA.java
+++ src/main/java/pt/ipleiria/estg/dei/ia/pl5/g13/warehouse/WarehouseProblemForGA.java
@@ -3,18 +3,16 @@ package pt.ipleiria.estg.dei.ia.pl5.g13.warehouse;
 import pt.ipleiria.estg.dei.ia.pl5.g13.ga.Problem;
 
 public class WarehouseProblemForGA implements Problem<WarehouseIndividual> {
-
     //TODO: this class might require the definition of additional methods and/or attributes
+    WarehouseAgentSearch agentSearch;
 
     public WarehouseProblemForGA(WarehouseAgentSearch agentSearch) {
-        //TODO:
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.agentSearch = agentSearch;
     }
 
     @Override
     public WarehouseIndividual getNewIndividual() {
-        //TODO:
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return new WarehouseIndividual(this, agentSearch.getNumProducts());
     }
 
     @Override
